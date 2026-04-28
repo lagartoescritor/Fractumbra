74611decb13e1bc1504722d6942b38daab80633b
Lagarto Escritor <lagartoescritor@gmail.com>
2026-04-27 23:38:25 -0300

    Update terminal.js
    Se añaden animaciones e interacciones para capítulo 5

--- terminal.js
+++ terminal.js
@@ -71,7 +71,7 @@ async function toggleFolder(folderId) {
 // Subrutina para la animación de borrado de la terminal y resistencia
 async function runResistAnimation(p) {
     const prefixText = " 🔹 Contenido: ";
-    const resistQuote = "\"El ciclo no se puede romper, porque ya fue escrito.\"";
+    const resistQuote = "\"El ciclo no puede romperse porque ya fue escrito.\"";
     const fullHTML = prefixText + `<span class="glitch-resist">${resistQuote}</span>`;
     
     while (p.closest('.folder').classList.contains('open')) {
@@ -90,7 +90,7 @@ async function runResistAnimation(p) {
         if (!p.closest('.folder').classList.contains('open')) break;
 
         await new Promise(r => setTimeout(r, 200));
-        p.innerHTML = prefixText + '<span class="error-text">[ ERROR DE PERSISTENCIA ]</span>';
+        p.innerHTML = prefixText + '<span class="error-text">[ ERROR: ARCHIVO PROTEGIDO POR LA ARQUITECTA ]</span>';
         await new Promise(r => setTimeout(r, 1200));
 
         // Restaura de golpe
